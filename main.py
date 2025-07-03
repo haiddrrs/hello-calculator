a3f63aa7870d85b65f5b66b6f114f73621b68d79
Haider Ali <haidersonly@gmail.com>
2025-07-03 20:01:17 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is a simple calculator program that performs addition and subtraction
+# It prompts the user for two numbers and then displays the results of the operations
+# It is designed to be user-friendly and straightforward
+
+print("Welcome to the Calculator!")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
